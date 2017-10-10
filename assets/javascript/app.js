510a6daaac04bcbfa0d73f7af6855027c2d75403
Amber Burroughs <amberlburroughs@gmail.com>
2017-10-10 10:13:48 -0700

    add zooming options

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 // test
-
-
+$(document).ready(function(){
 // Google analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
@@ -27,6 +26,7 @@ var connectedRef = database.ref(".info/connected");
 var hometownsRef = database.ref("/hometowns");
 // end firebase init
 
+var gmapsHometowns = [];
 // on load of map with no FB data 
 var hometownData = {
   /* in the format [city, latitude, longitude, population].
@@ -68,10 +68,12 @@ hometownsRef.on("value", function(snap) {
   // Display the viewer count in the html.
   // The number of online users is the number of children in the connections list.
   var hometowns = snap.val();
-  if(Object.keys(hometowns).length == 0){
-    var gmapsHometowns = [hometownData.defaultLocation];    
+  //console.log(hometowns);
+  //if(Object.keys(hometowns).length == 0)
+  if(!hometowns){
+    gmapsHometowns = [hometownData.defaultLocation];    
   } else {
-    var gmapsHometowns = formatHometownsGoogleMaps(hometowns);
+    gmapsHometowns = formatHometownsGoogleMaps(hometowns);
     hometownData.hometowns = gmapsHometowns;
   }
   renderMap(gmapsHometowns);
@@ -90,18 +92,279 @@ function formatHometownsGoogleMaps(locationObjs){
   return locations;
 }
 
-function renderMap(gmapsHometowns){
+// button that creates an event that zooms out automatically
+function renderMap(gmapsHometowns, zoomOut){
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: new google.maps.LatLng(gmapsHometowns[0][1], gmapsHometowns[0][2]),
+    zoom: (usersHometown.latitude)? (zoomOut ? zoomOut :11) : 3,
+    styles: [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#242f3e"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#746855"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#242f3e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.neighborhood",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#263c3f"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#6b9a76"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#38414e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#212a37"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9ca5b3"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#746855"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#1f2835"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#f3d19c"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#2f3948"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#d59563"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#17263c"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#515c6d"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#17263c"
+          }
+        ]
+      }
+    ],
+    center: new google.maps.LatLng(gmapsHometowns[gmapsHometowns.length - 1][1], gmapsHometowns[gmapsHometowns.length - 1][2]),
+    //center: new google.maps.LatLng(usersHometown.latitude, usersHometown.longitude),
+    disableDefaultUI: true,
+    zoomControl:true,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
-
+  console.log(gmapsHometowns);
+  console.log(gmapsHometowns[0][1], gmapsHometowns[0][2]);
   var infowindow = new google.maps.InfoWindow({});
 
   var marker, i;
 
   for (i = 0; i < gmapsHometowns.length; i++) {
+    
     marker = new google.maps.Marker({
       position: new google.maps.LatLng(gmapsHometowns[i][1], gmapsHometowns[i][2]),
       map: map
@@ -109,9 +372,9 @@ function renderMap(gmapsHometowns){
 
     google.maps.event.addListener(marker, 'click', (function (marker, i) {
       return function () {
-        map.setZoom(8);
+        map.setZoom(11);
         map.panTo(marker.position);
-        map.setCenter(marker);
+        // map.setCenter(marker);
         //console.log(gmapsHometowns[i][1] + "," + gmapsHometowns[i][2]);
         var weatherCoordinates = (gmapsHometowns[i][1] + "," + gmapsHometowns[i][2]).toString();
     
@@ -119,7 +382,7 @@ function renderMap(gmapsHometowns){
         var apiKey = "5be11e7282a5413f9ae194901170510";
         var baseURL =  "https://api.apixu.com/v1/current.json?";
         var queryURL = baseURL + "key=" + apiKey + "&q=" + queryPeram;
-        console.log(queryURL);
+        // console.log(queryURL);
         $.ajax({
           url: queryURL,
           method: "GET"
@@ -134,6 +397,7 @@ function renderMap(gmapsHometowns){
 
           infowindow.setContent(gmapsHometowns[i][0] + ' population: ' + gmapsHometowns[i][3] + 'Forecast: ' + weatherData);
           infowindow.open(map, marker);
+
         })
       }
     })(marker, i));
@@ -162,15 +426,14 @@ $(function (){
       getCityDetails(selectedObj.value).done(function (data) {
           // console.log(data);
           
-          var usersHometown = {
-            place: data.geobytesfqcn,
-            population: data.geobytespopulation,
-            latitude: data.geobyteslatitude,
-            longitude: data.geobyteslongitude
-          }
-
+        
+          usersHometown.place = data.geobytesfqcn;
+          usersHometown.population = data.geobytespopulation;
+          usersHometown.latitude = data.geobyteslatitude;
+          usersHometown.longitude = data.geobyteslongitude;
+          
           console.log(usersHometown);
-          handleHometownClick(usersHometown)
+          handleHometownClick(usersHometown);
           // after push hide form.
           return usersHometown;
         });
@@ -235,13 +498,28 @@ function getWeather(coordinates) {
   return 
 };
 
-$(document).ready(function(){
   // show the hometown form if and only if they don't
   // have the 'hide_hometown_form' field in local storage set
   if(localStorage.getItem('hide_hometown_form') === null){
     $('#hometown-form').removeClass('hide');
   }
+  var usersHometown = {};
 
+  $("#zoomOut").on("click", function(){
+    // usershometown into an array 
+      var userLocationData = []
+        Object.keys(usersHometown).forEach(function(key){
+            userLocationData.push(key);
+      });
+
+      renderMap((gmapsHometowns)? gmapsHometowns: userLocationData);
+      // usershometown into an array 
+  });
+  $("#downArrow").on("click", function() {
+      $("html, body").animate({
+      scrollTop: $("#main").offset().top}, 2000);
+    return false;
+    });
 
 });
 
